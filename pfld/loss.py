0b89083307403f8e6257562ad458ca1324ca5270
zhaozhichao <zhaozhichao4515@gmail.com>
2020-02-03 10:21:17 +0800

    add:smoothL1 loss and wing loss

--- pfld/loss.py
+++ pfld/loss.py
@@ -18,3 +18,23 @@ class PFLDLoss(nn.Module):
 
         l2_distant = torch.sum((landmark_gt - landmarks) * (landmark_gt - landmarks), axis=1)
         return torch.mean(weight_angle * weight_attribute * l2_distant), torch.mean(l2_distant)
+
+def smoothL1(y_true, y_pred, beta = 1):
+    """
+    very similar to the smooth_l1_loss from pytorch, but with
+    the extra beta parameter
+    """
+    mae = torch.abs(y_true - y_pred)
+    loss = torch.sum(torch.where(mae>beta, mae-0.5*beta , 0.5*mae**2/beta), axis=-1)
+    return torch.mean(loss)
+
+def wing_loss(y_true, y_pred, w=10.0, epsilon=2.0, N_LANDMARK = 106):
+    y_pred = y_pred.reshape(-1, N_LANDMARK, 2)
+    y_true = y_true.reshape(-1, N_LANDMARK, 2) 
+    
+    x = y_true - y_pred
+    c = w * (1.0 - math.log(1.0 + w / epsilon))
+    absolute_x = torch.abs(x)
+    losses = torch.where(w > absolute_x, w * torch.log(1.0 + absolute_x/epsilon), absolute_x - c)
+    loss = torch.mean(torch.sum(losses, axis=[1, 2]), axis=0)
+    return loss
