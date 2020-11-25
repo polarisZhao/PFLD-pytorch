cebe2e0ca570926a67721ca47617320f1b2ab6db
polarisZhao <zhaozhichao4515@gmail.com>
2020-11-25 10:40:03 +0800

    add checkpoint resume

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ from pfld.utils import AverageMeter
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 def print_args(args):
     for arg in vars(args):
         s = arg + ': ' + str(getattr(args, arg))
@@ -44,21 +45,23 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected')
 
 
-def train(train_loader, plfd_backbone, auxiliarynet, criterion, optimizer,
+def train(train_loader, pfld_backbone, auxiliarynet, criterion, optimizer,
           epoch):
     losses = AverageMeter()
 
+    weighted_loss, loss = None, None
     for img, landmark_gt, attribute_gt, euler_angle_gt in train_loader:
         img = img.to(device)
         attribute_gt = attribute_gt.to(device)
         landmark_gt = landmark_gt.to(device)
         euler_angle_gt = euler_angle_gt.to(device)
-        plfd_backbone = plfd_backbone.to(device)
+        pfld_backbone = pfld_backbone.to(device)
         auxiliarynet = auxiliarynet.to(device)
-        features, landmarks = plfd_backbone(img)
+        features, landmarks = pfld_backbone(img)
         angle = auxiliarynet(features)
-        weighted_loss, loss = criterion(attribute_gt, landmark_gt, euler_angle_gt,
-                                    angle, landmarks, args.train_batchsize)
+        weighted_loss, loss = criterion(attribute_gt, landmark_gt,
+                                        euler_angle_gt, angle, landmarks,
+                                        args.train_batchsize)
         optimizer.zero_grad()
         weighted_loss.backward()
         optimizer.step()
@@ -67,9 +70,9 @@ def train(train_loader, plfd_backbone, auxiliarynet, criterion, optimizer,
     return weighted_loss, loss
 
 
-def validate(wlfw_val_dataloader, plfd_backbone, auxiliarynet, criterion):
-    plfd_backbone.eval()
-    auxiliarynet.eval() 
+def validate(wlfw_val_dataloader, pfld_backbone, auxiliarynet, criterion):
+    pfld_backbone.eval()
+    auxiliarynet.eval()
     losses = []
     with torch.no_grad():
         for img, landmark_gt, attribute_gt, euler_angle_gt in wlfw_val_dataloader:
@@ -77,9 +80,9 @@ def validate(wlfw_val_dataloader, plfd_backbone, auxiliarynet, criterion):
             attribute_gt = attribute_gt.to(device)
             landmark_gt = landmark_gt.to(device)
             euler_angle_gt = euler_angle_gt.to(device)
-            plfd_backbone = plfd_backbone.to(device)
+            pfld_backbone = pfld_backbone.to(device)
             auxiliarynet = auxiliarynet.to(device)
-            _, landmark = plfd_backbone(img)
+            _, landmark = pfld_backbone(img)
             loss = torch.mean(torch.sum((landmark_gt - landmark)**2, axis=1))
             losses.append(loss.cpu().numpy())
     print("===> Evaluate:")
@@ -100,56 +103,64 @@ def main(args):
     print_args(args)
 
     # Step 2: model, criterion, optimizer, scheduler
-    plfd_backbone = PFLDInference().to(device)
+    pfld_backbone = PFLDInference().to(device)
     auxiliarynet = AuxiliaryNet().to(device)
     criterion = PFLDLoss()
-    optimizer = torch.optim.Adam(
-        [{
-            'params': plfd_backbone.parameters()
-        }, {
-            'params': auxiliarynet.parameters()
-        }],
-        lr=args.base_lr,
-        weight_decay=args.weight_decay)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=args.lr_patience, verbose=True)
+    optimizer = torch.optim.Adam([{
+        'params': pfld_backbone.parameters()
+    }, {
+        'params': auxiliarynet.parameters()
+    }],
+                                 lr=args.base_lr,
+                                 weight_decay=args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode='min', patience=args.lr_patience, verbose=True)
+    if args.resume:
+        checkpoint = torch.load(args.resume)
+        auxiliarynet.load_state_dict(checkpoint["auxiliarynet"])
+        pfld_backbone.load_state_dict(checkpoint["pfld_backbone"])
+        args.start_epoch = checkpoint["epoch"]
 
     # step 3: data
     # argumetion
     transform = transforms.Compose([transforms.ToTensor()])
     wlfwdataset = WLFWDatasets(args.dataroot, transform)
-    dataloader = DataLoader(
-        wlfwdataset,
-        batch_size=args.train_batchsize,
-        shuffle=True,
-        num_workers=args.workers,
-        drop_last=False)
+    dataloader = DataLoader(wlfwdataset,
+                            batch_size=args.train_batchsize,
+                            shuffle=True,
+                            num_workers=args.workers,
+                            drop_last=False)
 
     wlfw_val_dataset = WLFWDatasets(args.val_dataroot, transform)
-    wlfw_val_dataloader = DataLoader(
-        wlfw_val_dataset,
-        batch_size=args.val_batchsize,
-        shuffle=False,
-        num_workers=args.workers)
+    wlfw_val_dataloader = DataLoader(wlfw_val_dataset,
+                                     batch_size=args.val_batchsize,
+                                     shuffle=False,
+                                     num_workers=args.workers)
 
     # step 4: run
     writer = SummaryWriter(args.tensorboard)
     for epoch in range(args.start_epoch, args.end_epoch + 1):
-        weighted_train_loss, train_loss = train(dataloader, plfd_backbone, auxiliarynet,
-                                      criterion, optimizer, epoch)
-        filename = os.path.join(
-            str(args.snapshot), "checkpoint_epoch_" + str(epoch) + '.pth.tar')
-        save_checkpoint({
-            'epoch': epoch,
-            'plfd_backbone': plfd_backbone.state_dict(),
-            'auxiliarynet': auxiliarynet.state_dict()
-        }, filename)
+        weighted_train_loss, train_loss = train(dataloader, pfld_backbone,
+                                                auxiliarynet, criterion,
+                                                optimizer, epoch)
+        filename = os.path.join(str(args.snapshot),
+                                "checkpoint_epoch_" + str(epoch) + '.pth.tar')
+        save_checkpoint(
+            {
+                'epoch': epoch,
+                'pfld_backbone': pfld_backbone.state_dict(),
+                'auxiliarynet': auxiliarynet.state_dict()
+            }, filename)
 
-        val_loss = validate(wlfw_val_dataloader, plfd_backbone, auxiliarynet,
+        val_loss = validate(wlfw_val_dataloader, pfld_backbone, auxiliarynet,
                             criterion)
 
         scheduler.step(val_loss)
         writer.add_scalar('data/weighted_loss', weighted_train_loss, epoch)
-        writer.add_scalars('data/loss', {'val loss': val_loss, 'train loss': train_loss}, epoch)
+        writer.add_scalars('data/loss', {
+            'val loss': val_loss,
+            'train loss': train_loss
+        }, epoch)
     writer.close()
 
 
@@ -170,34 +181,36 @@ def parse_args():
 
     # -- epoch
     parser.add_argument('--start_epoch', default=1, type=int)
-    parser.add_argument('--end_epoch', default=1000, type=int)
+    parser.add_argument('--end_epoch', default=500, type=int)
 
     # -- snapshot、tensorboard log and checkpoint
+    parser.add_argument('--snapshot',
+                        default='./checkpoint/snapshot/',
+                        type=str,
+                        metavar='PATH')
+    parser.add_argument('--log_file',
+                        default="./checkpoint/train.logs",
+                        type=str)
+    parser.add_argument('--tensorboard',
+                        default="./checkpoint/tensorboard",
+                        type=str)
     parser.add_argument(
-        '--snapshot',
-        default='./checkpoint/snapshot/',
+        '--resume',
+        default='./checkpoint/snapshot/checkpoint_epoch_308.pth.tar',
         type=str,
         metavar='PATH')
-    parser.add_argument(
-        '--log_file', default="./checkpoint/train.logs", type=str)
-    parser.add_argument(
-        '--tensorboard', default="./checkpoint/tensorboard", type=str)
-    parser.add_argument(
-        '--resume', default='', type=str, metavar='PATH')  # TBD
 
     # --dataset
-    parser.add_argument(
-        '--dataroot',
-        default='./data/train_data/list.txt',
-        type=str,
-        metavar='PATH')
-    parser.add_argument(
-        '--val_dataroot',
-        default='./data/test_data/list.txt',
-        type=str,
-        metavar='PATH')
+    parser.add_argument('--dataroot',
+                        default='./data/train_data/list.txt',
+                        type=str,
+                        metavar='PATH')
+    parser.add_argument('--val_dataroot',
+                        default='./data/test_data/list.txt',
+                        type=str,
+                        metavar='PATH')
     parser.add_argument('--train_batchsize', default=256, type=int)
-    parser.add_argument('--val_batchsize', default=8, type=int)
+    parser.add_argument('--val_batchsize', default=256, type=int)
     args = parser.parse_args()
     return args
 
